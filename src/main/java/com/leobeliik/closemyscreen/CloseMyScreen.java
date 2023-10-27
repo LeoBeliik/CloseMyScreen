07c17a0c71e11474723d1c5938fb75adcd9d5e13
Leo <Leobrac23@gmail.com>
2023-10-27 20:54:36 -0300

    I may have overdone this... Oh, well

--- src/main/java/com/leobeliik/closemyscreen/CloseMyScreen.java
+++ src/main/java/com/leobeliik/closemyscreen/CloseMyScreen.java
@@ -1,33 +1,78 @@
 package com.leobeliik.closemyscreen;
 
+import com.google.common.reflect.TypeToken;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.stream.JsonReader;
+import com.mojang.blaze3d.platform.InputConstants;
+import net.minecraft.client.KeyMapping;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.components.EditBox;
 import net.minecraft.client.gui.components.events.GuiEventListener;
 import net.minecraft.client.gui.screens.Screen;
-import net.minecraft.client.gui.screens.inventory.PageButton;
+import net.minecraft.network.chat.Component;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
+import net.minecraftforge.client.event.RegisterKeyMappingsEvent;
 import net.minecraftforge.client.event.ScreenEvent;
 import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.entity.player.PlayerEvent;
 import net.minecraftforge.eventbus.api.EventPriority;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.DistExecutor;
 import net.minecraftforge.fml.IExtensionPoint;
 import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.List;
 
 // The value here should match an entry in the META-INF/mods.toml file
 @Mod(CloseMyScreen.MODID)
 public class CloseMyScreen {
     // Define mod id in a common place for everything to reference
     static final String MODID = "closemyscreen";
+    private static final Gson gson = new GsonBuilder().setPrettyPrinting().create();
+
+    private static final KeyMapping screenKey = screenKey();
+    private static List<String> noNoScrens = new ArrayList<>();
 
     public CloseMyScreen() {
+        noNoScrens.clear();
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
         MinecraftForge.EVENT_BUS.register(this);
         ModLoadingContext.get().registerExtensionPoint(IExtensionPoint.DisplayTest.class,
                 () -> new IExtensionPoint.DisplayTest(() -> "", (a, b) -> true));
+        DistExecutor.unsafeRunWhenOn(Dist.CLIENT, () -> () -> modEventBus.addListener(this::keyRegistry));
+        noNoScrens.add("BookEditScreen");
+        loadScreens().stream().filter(loadScreen -> !noNoScrens.contains(loadScreen)).forEach(loadScreen -> noNoScrens.add(loadScreen));
+    }
+
+    private static List<String> loadScreens() {
+        try (InputStreamReader reader = new InputStreamReader(new FileInputStream("closeMyScreen/doNotClose.dat"), StandardCharsets.UTF_8)) {
+            return gson.fromJson(new JsonReader(reader), new TypeToken<List<String>>() {
+            }.getType());
+        } catch (Exception e) {
+            return noNoScrens;
+        }
+    }
+
+    @OnlyIn(Dist.CLIENT)
+    private void keyRegistry(final RegisterKeyMappingsEvent event) {
+        event.register(screenKey);
+    }
+
+    @OnlyIn(Dist.CLIENT)
+    private static KeyMapping screenKey() {
+        return new KeyMapping(
+                String.valueOf("Don't close screen"),
+                InputConstants.Type.KEYSYM,
+                InputConstants.UNKNOWN.getValue(),
+                "key.categories.misc");
     }
 
     @OnlyIn(Dist.CLIENT)
@@ -35,14 +80,21 @@ public class CloseMyScreen {
     public void onKeyInput(ScreenEvent.KeyPressed.Pre event) {
         Screen screen = event.getScreen();
         Minecraft minecraft = screen.getMinecraft();
-        if (minecraft.level != null && minecraft.options.keyInventory.matches(event.getKeyCode(), event.getScanCode())) {
-            for (GuiEventListener renderable : screen.children()) {
-                if (renderable instanceof EditBox searchBar && searchBar.canConsumeInput() || renderable instanceof PageButton) {
-                    return;
+        String screenClass = screen.getClass().getName().split("[.]")[screen.getClass().getName().split("[.]").length - 1];
+
+        if (screenKey.matches(event.getKeyCode(), event.getScanCode())) {
+            noNoScrens.add(screenClass);
+            minecraft.player.sendSystemMessage(Component.nullToEmpty("Screen won't close with the Inv button"));
+        } else if (!noNoScrens.contains(screenClass)) {
+            if (minecraft.level != null && minecraft.options.keyInventory.matches(event.getKeyCode(), event.getScanCode())) {
+                for (GuiEventListener renderable : screen.children()) {
+                    if (renderable instanceof EditBox searchBar && searchBar.canConsumeInput()) {
+                        return;
+                    }
                 }
+                screen.onClose();
+                event.setCanceled(true);
             }
-            screen.onClose();
-            event.setCanceled(true);
         }
     }
 
@@ -60,5 +112,18 @@ public class CloseMyScreen {
                 }
             }
         }
+    }
+
+    @OnlyIn(Dist.CLIENT)
+    @SubscribeEvent()
+    public void onPlayerLogout(PlayerEvent.PlayerLoggedOutEvent event) {
+        saveScreens();
+    }
+
+    private static void saveScreens() {
+        new File("closeMyScreen/").mkdirs();
+        try (Writer writer = new OutputStreamWriter(new FileOutputStream("closeMyScreen/doNotClose.dat"), StandardCharsets.UTF_8)) {
+            writer.write(gson.toJson(noNoScrens));
+        } catch (IOException ignored) {}
     }
 }
